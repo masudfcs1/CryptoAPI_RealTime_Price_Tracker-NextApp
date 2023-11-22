e429a93623de3ea7af7eafbac29d46037511f0e7
masud rana <masudfcs1@gmail.com>
2023-11-22 19:37:41 +0600

    push

--- utils/hooks.js
+++ utils/hooks.js
@@ -4,47 +4,43 @@ import { getSymbols, findByValue } from "../utils";
 import { CRYPTOCURRENCIES } from "../configs";
 
 const useTicker = () => {
-    const [cryptocurrencies, setcryptocurrencies] = useState(CRYPTOCURRENCIES);
+  const [cryptocurrencies, setcryptocurrencies] = useState(CRYPTOCURRENCIES);
 
-    const fetchCrypto = useCallback(async() => {
-        try {
-            const response = await fetch(
-                `https://api.binance.com/api/v3/ticker/24hr?symbols=${JSON.stringify(
+  const fetchCrypto = useCallback(async () => {
+    try {
+      const response = await fetch(
+        `https://api.binance.com/api/v3/ticker/24hr?symbols=${JSON.stringify(
           getSymbols()
         )} `
-            );
+      );
 
-            const data = await response.json();
+      const data = await response.json();
 
-            setcryptocurrencies(
-                cryptocurrencies.map((item) => {
-                    const { lastPrice, lowPrice, highPrice } = findByValue(data, item.symbol) || {}
+      setcryptocurrencies(
+        cryptocurrencies.map((item) => {
+          const { lastPrice, lowPrice, highPrice } =
+            findByValue(data, item.symbol) || {};
 
-                    return {
-                        ...item,
-                        highPrice,
-                        lowPrice,
-                        price: lastPrice,
-                        prevPrice: item ? .price || 0,
-                    }
-                })
-            );
+          return {
+            ...item,
+            highPrice,
+            lowPrice,
+            price: lastPrice,
+            prevPrice: item.price || 0
+          };
+        })
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }, [cryptocurrencies]);
 
-
-
-
-        } catch (error) {
-            console.log(error);
-        }
-    }, [cryptocurrencies]);
-
-
-    useEffect(() => {
-        const interval = setInterval(fetchCrypto, 5000)
-        return () => clearInterval(interval)
-    })
-    return cryptocurrencies
-        // fetchCrypto();
+  useEffect(() => {
+    const interval = setInterval(fetchCrypto, 5000);
+    return () => clearInterval(interval);
+  });
+  return cryptocurrencies;
+  // fetchCrypto();
 };
 
 export { useTicker };
